e1fd13e9c9d94a092b841c5250ddf1bff283bd3d
mcginav <mcginav@yahoo.com>
2020-12-05 10:52:22 -0800

    Adding the election audit code.

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,33 @@
-print("Hello World")
+counties=["Arapahoe","Denver","Jefferson"]
+if counties[1]=='Denver':
+    print(counties[1])
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")   
+
+for county in counties:
+    print (county)
+counties_dict={"Araoahoe":422829,"Denver":463353, "Jefferson":432438}
+for county in counties_dict:
+    print(county)    
+for voters in counties_dict.values():
+    print(voters)
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
+
+voting_data = {"county":"Arapahoe", "registered_voters": 422829}, 
+{"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}
+for county,registered_voters in voting_data:
+    print(f"{county} county has {registered_voters:,} registered voters.")
+    
+    
--- pyPoll.py
+++ pyPoll.py
@@ -15,8 +15,76 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file
 election_data = open(file_to_load,'r')
+
+#1. Initialize a total vote counter
+total_votes=0
+
+# Candidate Options
+candidate_options = []
+#1. Declare the empty dictionary
+candidate_votes = {}
+#candidate_votes={"candidate_name1":votes, "candidate_name2":votes,"candidate_name3":votes}
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
     
-    # Print the file object.
-    print(election_data)
+   # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+    # Print the candidate name from each row    
+        candidate_name = row[2]
+                
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+                
+        # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+        #2. Begin tracking that candidate's vote count.    
+            candidate_votes[candidate_name] = 0
+    # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1 
+# Determine the percentage of votes for each candidate by looping through the counts
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes)/float(total_votes)* 100
+
+        # Print out each candidate's name, vote count, and percentage of votes
+        print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+
+        # Determine winning vote count and candidate
+        # Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+             # If true, then set winning_count = votes and winning_percent = # of vote_percentage.
+             winning_count = votes
+             winning_percentage = vote_percentage
+             # Set the winning_candidate equal to the candidate's name.
+             winning_candidate = candidate_name
+
+# Print the winning candidate, vote count, and percentage of votes.
+winning_candidate_summary =(
+        f"---------------------\n"
+        f"Winner:{winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"---------------------\n")  
+            
+# print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")  
+print(winning_candidate_summary) 
+# Print the candidate list.
+#print (candidate_votes)    
 
